2345a8120b73045c92f1773dadf171762565bd22
Denny Angesti Pratama <dennyangesti@gmail.com>
2021-03-26 15:01:20 +0700

    change server

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = function (app) {
-   const BASE_URL = 'http://ec2-3-19-209-157.us-east-2.compute.amazonaws.com:8080'
+   const BASE_URL = 'http://ec2-3-131-133-210.us-east-2.compute.amazonaws.com:8080'
    app.use(
       '/configurations',
       createProxyMiddleware({
